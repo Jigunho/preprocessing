5508be4a5de8720335fcdab59abdb1db489eaa58
jigeonho <jce0831@gmail.com>
2019-10-17 15:49:06 +0900

    config modified

--- 3_makegrid.js
+++ 3_makegrid.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const config = require('./config');
-const date = config.date;
 const folder = `./location`;
 const outerFolder = `./final`;
 const locationName = config.location
--- 4_find_time.js
+++ 4_find_time.js
@@ -7,45 +7,44 @@
 // const dates = ['20190819', '20190820', '20190821'];
 
 const config = require('./config_2');
-const dates = config.dates;
+// const dates = config.dates;
 const timezones = config.timezones;
-let days = dates.length;
+// let days = dates.length;
 const location = config.location;
 
 
 const fs = require('fs');
 const folder = './final'
-dates.forEach(function (date) {
 
-  fs.readdir(`${folder}/${location}/`, function (err, files) {
-    let result = {};
-    files.forEach(file => {
-      let datas = fs.readFileSync(`${folder}/${location}/${file}`, 'utf8');
-      let rows = datas.split('\n');
-      for (let j = 0; j < rows.length; j++) {
-        let cols = rows[j].split('\t');
-        let timestamp = parseInt(cols[1]);
-        let hour = new Date(timestamp).getHours();
+fs.readdir(`${folder}/${location}/`, function (err, files) {
+  files.forEach(file => {
 
-        for (let k = 0; k < days; k++) {
-          if (timezones[k].includes(hour)) {
-            fs.appendFileSync(`${location}_time_zone${k}.txt`, `${rows[j]}\n`);
-          }
+    console.log(file);
+    let datas = fs.readFileSync(`${folder}/${location}/${file}`, 'utf8');
+    let rows = datas.split('\n');
+    for (let j = 0; j < rows.length; j++) {
+      let cols = rows[j].split('\t');
+      let timestamp = parseInt(cols[1]);
+      let hour = new Date(timestamp).getHours();
+
+      for (let k = 0; k < timezones.length; k++) {
+        if (timezones[k].includes(hour)) {
+          fs.appendFileSync(`${location}_time_zone${k}.txt`, `${rows[j]}\n`);
         }
-
-        // if (timezone1.includes(hour)) {
-        //   fs.appendFileSync(`${location}_time_zone111.txt`, `${rows[j]}\n`);
-        // }
-        // else if (timezone2.includes(hour)) {
-        //   fs.appendFileSync(`${location}_time_zone222.txt`, `${rows[j]}\n`);
-        // }
-        // else if (timezone3.includes(hour)) {
-        //   fs.appendFileSync(`${location}_time_zone333.txt`, `${rows[j]}\n`);
-        // }
       }
-    })
 
-  });
+      // if (timezone1.includes(hour)) {
+      //   fs.appendFileSync(`${location}_time_zone111.txt`, `${rows[j]}\n`);
+      // }
+      // else if (timezone2.includes(hour)) {
+      //   fs.appendFileSync(`${location}_time_zone222.txt`, `${rows[j]}\n`);
+      // }
+      // else if (timezone3.includes(hour)) {
+      //   fs.appendFileSync(`${location}_time_zone333.txt`, `${rows[j]}\n`);
+      // }
+    }
+  })
+
   // for (let d = 0; d < subdirs.length; d++) {
   //   console.log(subdirs[d]);
 
