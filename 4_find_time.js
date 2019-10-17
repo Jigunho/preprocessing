f2e9b8166e96d74504ae6a70720db1d76433dbe5
jigeonho <jce0831@gmail.com>
2019-10-17 15:36:47 +0900

    config 분리

--- 4_find_time.js
+++ 4_find_time.js
@@ -1,15 +1,21 @@
-const config = require('./config');
-const location = config.location;
-const timezone1 = [3, 4, 5];
-const timezone2 = [9, 10, 11];
-const timezone3 = [17, 18, 19];
+// const config = require('./config');
+// const location = config.location;
+// const timezone1 = [3, 4, 5];
+// const timezone2 = [9, 10, 11];
+// const timezone3 = [17, 18, 19];
 
-const dates = ['20190819', '20190820', '20190821'];
+// const dates = ['20190819', '20190820', '20190821'];
+
+const config = require('./config_2');
+const dates = config.dates;
+const timezones = config.timezones;
+let days = dates.length;
+const location = config.location;
 
 
 const fs = require('fs');
 const folder = './final'
-dates.forEach(function(date) {
+dates.forEach(function (date) {
 
   fs.readdir(`${folder}/${location}/`, function (err, files) {
     let result = {};
@@ -20,20 +26,26 @@ dates.forEach(function(date) {
         let cols = rows[j].split('\t');
         let timestamp = parseInt(cols[1]);
         let hour = new Date(timestamp).getHours();
-  
-        if (timezone1.includes(hour)) {
-          fs.appendFileSync(`${location}_time_zone111.txt`, `${rows[j]}\n`);
+
+        for (let k = 0; k < days; k++) {
+          if (timezones[k].includes(hour)) {
+            fs.appendFileSync(`${location}_time_zone${k}.txt`, `${rows[j]}\n`);
+          }
         }
-        else if (timezone2.includes(hour)) {
-          fs.appendFileSync(`${location}_time_zone222.txt`, `${rows[j]}\n`);
-        }
-        else if (timezone3.includes(hour)) {
-          fs.appendFileSync(`${location}_time_zone333.txt`, `${rows[j]}\n`);
-        }
+
+        // if (timezone1.includes(hour)) {
+        //   fs.appendFileSync(`${location}_time_zone111.txt`, `${rows[j]}\n`);
+        // }
+        // else if (timezone2.includes(hour)) {
+        //   fs.appendFileSync(`${location}_time_zone222.txt`, `${rows[j]}\n`);
+        // }
+        // else if (timezone3.includes(hour)) {
+        //   fs.appendFileSync(`${location}_time_zone333.txt`, `${rows[j]}\n`);
+        // }
       }
     })
-  
-});
+
+  });
   // for (let d = 0; d < subdirs.length; d++) {
   //   console.log(subdirs[d]);
 
---
+++ config_2.js
@@ -0,0 +1,5 @@
+module.exports = {
+  dates: ['20190819','20190820','20190821'], // 20190820_after, 20190821_after
+  timezones: [[1,2,3], [1,2,3],[1,23]],
+  location: 'seoul2',
+}
